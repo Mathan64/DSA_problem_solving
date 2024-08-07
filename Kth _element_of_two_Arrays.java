208d9b5f455b3a0ffc0bf2c400f5cae9ea237aff
mathan <142643402+Mathan64@users.noreply.github.com>
2024-08-07 21:39:40 +0530

    Update Kth _element_of_two_Arrays.java

--- Kth _element_of_two_Arrays.java
+++ Kth _element_of_two_Arrays.java
@@ -12,12 +12,13 @@
         return list.get(k-1);
     }
 /*test cases
-  Examples :
-Input: n = 2
-Output: 1 
-Explanation: 1 is the 2nd number of fibonacci series.
-Input: n = 5
-Output: 5
-Explanation: 5 is the 5th number of fibonacci series.
+Examples :
+
+Input: k = 5, arr1[] = [2, 3, 6, 7, 9], arr2[] = [1, 4, 8, 10]
+Output: 6
+Explanation: The final combined sorted array would be - 1, 2, 3, 4, 6, 7, 8, 9, 10. The 5th element of this array is 6.
+Input: k = 7, arr1[] = [100, 112, 256, 349, 770], arr2[] = [72, 86, 113, 119, 265, 445, 892]
+Output: 256
+Explanation: Combined sorted array is - 72, 86, 100, 112, 113, 119, 256, 265, 349, 445, 770, 892. 7th element of this array is 256.
   */
   
