ae7332e12d6673710852a110395ad57869487dea
mathan <142643402+Mathan64@users.noreply.github.com>
2024-09-02 22:14:46 +0530

    Create Union of Two Sorted Arrays.java

---
+++ Union of Two Sorted Arrays.java
@@ -0,0 +1,46 @@
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        // add your code here
+        TreeSet<Integer> ts=new TreeSet<>();
+        for(int i=0;i<n;i++)
+            ts.add(arr1[i]);
+        
+        for(int i=0;i<m;i++)
+            ts.add(arr2[i]);
+        ArrayList<Integer> al=new ArrayList<>(ts);
+        return al;
+    }
+}
+
+/*
+Example 1:
+
+Input: 
+n = 5, arr1[] = {1, 2, 3, 4, 5}  
+m = 5, arr2 [] = {1, 2, 3, 6, 7}
+Output: 
+1 2 3 4 5 6 7
+Explanation: 
+Distinct elements including both the arrays are: 1 2 3 4 5 6 7.
+Example 2:
+
+Input: 
+n = 5, arr1[] = {2, 2, 3, 4, 5}  
+m = 5, arr2[] = {1, 1, 2, 3, 4}
+Output: 
+1 2 3 4 5
+Explanation: 
+Distinct elements including both the arrays are: 1 2 3 4 5.
+Example 3:
+
+Input:
+n = 5, arr1[] = {1, 1, 1, 1, 1}
+m = 5, arr2[] = {2, 2, 2, 2, 2}
+Output: 
+1 2
+Explanation: 
+Distinct elements including both the arrays are: 1 2.
+*/
