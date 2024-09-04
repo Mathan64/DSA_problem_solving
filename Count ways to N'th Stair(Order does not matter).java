54b640416368196fc6b11854431309d49e8aa605
mathan <142643402+Mathan64@users.noreply.github.com>
2024-09-04 22:02:09 +0530

    Create Count ways to N'th Stair(Order does not matter).java

---
+++ Count ways to N'th Stair(Order does not matter).java
@@ -0,0 +1,17 @@
+class Solution {
+    public int nthStair(int n) {
+        // Code here
+        return n/2 +1;
+    }
+}
+
+/*
+Examples :
+
+Input: n = 4
+Output: 3
+Explanation: Three ways to reach at 4th stair. They are {1, 1, 1, 1}, {1, 1, 2}, {2, 2}.
+Input: n = 5
+Output: 3
+Explanation: Three ways to reach at 5th stair. They are {1, 1, 1, 1, 1}, {1, 1, 2, 1} and {1, 2, 2}.
+*/
